eae659501b99d4dd80108a9483d48c5bd5d85566
Yunier Alvarez <yunieralvarez@gmail.com>
2025-02-15 20:16:49 -0500

    auto sync from primary repo

--- src/tree.sh
+++ src/tree.sh
@@ -1,4 +1,5 @@
 touch ~/.dotfiles/src/tree/tree-filen-before.txt
+tree -a ~/.config/@filen > ~/.dotfiles/src/tree/tree-filen-after.txt
 tree -aDgNpqsu ~/.config/@filen >> ~/.dotfiles/src/tree/tree-filen-after.txt
 
 tree . -a -I "node_modules" > ~/.dotfiles/src/tree/tree-output.txt
