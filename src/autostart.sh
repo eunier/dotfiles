010cce5bdf42ca0742026d7bd43ca442c120d5b0
Yunier Alvarez <yunieralvarez@gmail.com>
2025-02-15 19:52:02 -0500

    auto sync from primary repo

--- src/autostart.sh
+++ src/autostart.sh
@@ -1,3 +1,3 @@
 tree ~/.config/autostart > ~/.dotfiles/src/tree/tree-autostart-after.txt
 
-tree -a -D -g -N -p -q -s -u ~/.config/autostart >> ~/.dotfiles/src/tree/tree-autostart-after.txt
+tree -aDgNpqsu ~/.config/autostart >> ~/.dotfiles/src/tree/tree-autostart-after.txt
