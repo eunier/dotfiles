ab6573d1b3e7075078b68cf6a262eda71ebe9557
Yunier Alvarez <yunieralvarez@gmail.com>
2025-02-17 23:27:11 -0500

    auto sync from primary repo

--- src/yadm.sh
+++ src/yadm.sh
@@ -6,7 +6,10 @@ yadm add ~/.gitconfig
 yadm add ~/.config/Code/User/settings.json
 yadm add ~/.config/Code/User/keybindings.json
 yadm add ~/.dotfiles
+yadm add ~/Projects/com/gitlab/yunieralvarez/repo/zig/top-down-game/TODO.md
+
 cd ~ || exit
 yadm ls-files >~/.dotfiles/src/yadm/yadm-ls-files.txt
 cd ~/.dotfiles || exit
+
 yadm commit -m "sync"
