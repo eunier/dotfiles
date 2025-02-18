49d433ae7a0dbad35ca2f4d5e682eea583b7a661
Yunier Alvarez <yunieralvarez@gmail.com>
2025-02-17 21:44:04 -0500

    auto sync from primary repo

--- src/diff/diff-flatpak.sh
+++ src/diff/diff-flatpak.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Diffing flatpak-list..."
+echo "Diffing flatpak..."
 diff --color ~/.dotfiles/src/flatpak/flatpak-list-before.txt ~/.dotfiles/src/flatpak/flatpak-list-after.txt
 diff --color ~/.dotfiles/src/flatpak-progress-before.txt ~/.dotfiles/src/flatpak-progress-after.txt
