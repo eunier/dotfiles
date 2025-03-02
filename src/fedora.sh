d7e44d3ca2fca8b418bba50c7756ec000e49ca43
Yunier Alvarez <yunieralvarez@gmail.com>
2025-03-02 18:21:29 -0500

    auto sync from primary repo

--- src/fedora.sh
+++ src/fedora.sh
@@ -13,19 +13,19 @@ sh ~/.dotfiles/src/ollama.sh
 sh ~/.dotfiles/src/zig.sh
 sh ~/.dotfiles/src/git.sh
 
-sh ~/.dotfiles/src/autostart.sh
-sh ~/.dotfiles/src/dconf.sh
-sh ~/.dotfiles/src/doas.sh
-sh ~/.dotfiles/src/sync.sh
-sh ~/.dotfiles/src/syncthing.sh
-sh ~/.dotfiles/src/timeshift.sh
-sh ~/.dotfiles/src/tree.sh
-sh ~/.dotfiles/src/vscode.sh
-sh ~/.dotfiles/src/gitlab.sh
+# sh ~/.dotfiles/src/autostart.sh
+# sh ~/.dotfiles/src/dconf.sh
+# sh ~/.dotfiles/src/doas.sh
+# sh ~/.dotfiles/src/sync.sh
+# sh ~/.dotfiles/src/syncthing.sh
+# sh ~/.dotfiles/src/timeshift.sh
+# sh ~/.dotfiles/src/tree.sh
+# sh ~/.dotfiles/src/vscode.sh
+# sh ~/.dotfiles/src/gitlab.sh
 
 # sudo timeshift --check
 
-sh ~/.dotfiles/src/diff.sh
-sh ~/.dotfiles/src/git/git-sync-repo.sh
+# sh ~/.dotfiles/src/diff.sh
+# sh ~/.dotfiles/src/git/git-sync-repo.sh
 
 fastfetch
