ef2993db985dc5c863beec7a047c7bbee6f9066a
Yunier Alvarez <yunieralvarez@gmail.com>
2025-03-02 18:24:32 -0500

    auto sync from primary repo

--- src/dnf.sh
+++ src/dnf.sh
@@ -12,16 +12,14 @@ if ! dnf repolist | grep -q "LibreWolf Software Repository"; then
     sudo curl -fsSL https://repo.librewolf.net/librewolf.repo | pkexec tee /etc/yum.repos.d/librewolf.repo
 fi
 
-
 if ! dnf repolist | grep -q "Mullvad VPN"; then
     sudo dnf config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/stable/mullvad.repo
 fi
 
 if ! dnf repolist | grep -q "Visual Studio Code"; then
     sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
 fi
-
 
 if ! command -v beaver-notes /dev/null 2>&1; then
     echo -e "\e[31mDownload 'beaver-notes' from 'https://beavernotes.com/#/Download'\e[0m"
@@ -30,7 +28,6 @@ fi
 if ! command -v google-chrome-stable /dev/null 2>&1; then
     echo -e "\e[31mDownload 'google-chrome-stable' from 'https://www.google.com/chrome\e[0m"
 fi
-
 
 if ! command -v @filendesktop /dev/null 2>&1; then
     echo -e "\e[31mDownload '@filendesktop' from 'https://filen.io/products/desktop\e[0m and installed with dnf"
@@ -60,6 +57,7 @@ sudo dnf install \
     glab \
     gnome-boxes \
     gnome-builder \
+    gnome-tweaks \
     google-chrome-stable \
     jetbrains-mono-fonts \
     jetbrains-mono-nl-fonts \
@@ -81,10 +79,10 @@ sudo dnf install \
     vim
 
 touch ~/.dotfiles/src/dnf/dnf-info-installed-before.txt
-dnf repoquery --info --installed > ~/.dotfiles/src/dnf/dnf-info-installed-after.txt
+dnf repoquery --info --installed >~/.dotfiles/src/dnf/dnf-info-installed-after.txt
 
 touch ~/.dotfiles/src/dnf/dnf-repo-list-before.txt
-dnf repolist > ~/.dotfiles/src/dnf/dnf-repo-list-after.txt
+dnf repolist >~/.dotfiles/src/dnf/dnf-repo-list-after.txt
 
 touch ~/.dotfiles/src/dnf/dnf-copr-list-before.txt
-dnf copr list > ~/.dotfiles/src/dnf/dnf-copr-list-after.txt
+dnf copr list >~/.dotfiles/src/dnf/dnf-copr-list-after.txt
