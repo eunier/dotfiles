eb68e2e37709fed191a0a3d132717122ea08f12b
Yunier Alvarez <yunieralvarez@gmail.com>
2025-03-02 22:26:40 -0500

    auto sync from primary repo

--- src/vscode.sh
+++ src/vscode.sh
@@ -2,41 +2,41 @@
 
 code --update-extensions
 
-code --install-extension aswinkumar863.sort-editors
-code --install-extension biomejs.biome
-code --install-extension chrisdias.vscode-opennewinstance
-code --install-extension coddx.coddx-alpha
-code --install-extension DavidAnson.vscode-markdownlint
-code --install-extension EditorConfig.EditorConfig
-code --install-extension egirlcatnip.adwaita-github-theme
-code --install-extension esbenp.prettier-vscode
-code --install-extension evan-buss.font-switcher
-code --install-extension Gruntfuggly.todo-tree
-code --install-extension iciclesoft.workspacesort
-code --install-extension JannisX11.batch-rename-extension
-code --install-extension maattdd.gitless
-code --install-extension mads-hartmann.bash-ide-vscode
-code --install-extension maptz.regionfolder
-code --install-extension MariusAlchimavicius.json-to-ts
-code --install-extension mkhl.shfmt
-code --install-extension ms-vscode.cpptools-extension-pack
-code --install-extension orhun.last-commit
-code --install-extension oven.bun-vscode
-code --install-extension pflannery.vscode-versionlens
-code --install-extension piousdeer.adwaita-theme
-code --install-extension PKief.copy-branch-name
-code --install-extension PKief.material-icon-theme
-code --install-extension richie5um2.vscode-sort-json
-code --install-extension rjmacarthy.twinny
-code --install-extension rob-bennett.workspaceWizard
-code --install-extension rpinski.shebang-snippets
-code --install-extension rust-lang.rust-analyzer
-code --install-extension stkb.rewrap
-code --install-extension streetsidesoftware.code-spell-checker
-code --install-extension timonwong.shellcheck
-code --install-extension vadimcn.vscode-lldb
-code --install-extension zhuangtongfa.Material-theme
-code --install-extension ziglang.vscode-zig
+code --install-extension aswinkumar863.sort-editors >/dev/null 2>&1 && printf "."
+code --install-extension biomejs.biome >/dev/null 2>&1 && printf "."
+code --install-extension chrisdias.vscode-opennewinstance >/dev/null 2>&1 && printf "."
+code --install-extension coddx.coddx-alpha >/dev/null 2>&1 && printf "."
+code --install-extension DavidAnson.vscode-markdownlint >/dev/null 2>&1 && printf "."
+code --install-extension EditorConfig.EditorConfig >/dev/null 2>&1 && printf "."
+code --install-extension egirlcatnip.adwaita-github-theme >/dev/null 2>&1 && printf "."
+code --install-extension esbenp.prettier-vscode >/dev/null 2>&1 && printf "."
+code --install-extension evan-buss.font-switcher >/dev/null 2>&1 && printf "."
+code --install-extension Gruntfuggly.todo-tree >/dev/null 2>&1 && printf "."
+code --install-extension iciclesoft.workspacesort >/dev/null 2>&1 && printf "."
+code --install-extension JannisX11.batch-rename-extension >/dev/null 2>&1 && printf "."
+code --install-extension maattdd.gitless >/dev/null 2>&1 && printf "."
+code --install-extension mads-hartmann.bash-ide-vscode >/dev/null 2>&1 && printf "."
+code --install-extension maptz.regionfolder >/dev/null 2>&1 && printf "."
+code --install-extension MariusAlchimavicius.json-to-ts >/dev/null 2>&1 && printf "."
+code --install-extension mkhl.shfmt >/dev/null 2>&1 && printf "."
+code --install-extension ms-vscode.cpptools-extension-pack >/dev/null 2>&1 && printf "."
+code --install-extension orhun.last-commit >/dev/null 2>&1 && printf "."
+code --install-extension oven.bun-vscode >/dev/null 2>&1 && printf "."
+code --install-extension pflannery.vscode-versionlens >/dev/null 2>&1 && printf "."
+code --install-extension piousdeer.adwaita-theme >/dev/null 2>&1 && printf "."
+code --install-extension PKief.copy-branch-name >/dev/null 2>&1 && printf "."
+code --install-extension PKief.material-icon-theme >/dev/null 2>&1 && printf "."
+code --install-extension richie5um2.vscode-sort-json >/dev/null 2>&1 && printf "."
+code --install-extension rjmacarthy.twinny >/dev/null 2>&1 && printf "."
+code --install-extension rob-bennett.workspaceWizard >/dev/null 2>&1 && printf "."
+code --install-extension rpinski.shebang-snippets >/dev/null 2>&1 && printf "."
+code --install-extension rust-lang.rust-analyzer >/dev/null 2>&1 && printf "."
+code --install-extension stkb.rewrap >/dev/null 2>&1 && printf "."
+code --install-extension streetsidesoftware.code-spell-checker >/dev/null 2>&1 && printf "."
+code --install-extension timonwong.shellcheck >/dev/null 2>&1 && printf "."
+code --install-extension vadimcn.vscode-lldb >/dev/null 2>&1 && printf "."
+code --install-extension zhuangtongfa.Material-theme >/dev/null 2>&1 && printf "."
+code --install-extension ziglang.vscode-zig >/dev/null 2>&1 && printf "."
 
 touch ~/.dotfiles/src/vscode/vscode-extensions-before.txt
 code --list-extensions >~/.dotfiles/src/vscode/vscode-extensions-after.txt
@@ -46,7 +46,6 @@ sh ~/.dotfiles/src/vscode/vscode-font-switch.sh
 touch ~/.config/Code/User/settings.json
 touch ~/.dotfiles/src/vscode/vscode-settings-before.jsonc
 cp ~/.config/Code/User/settings.json ~/.dotfiles/src/vscode/vscode-settings-after.jsonc
-
 
 touch ~/.config/Code/User/keybindings.json
 touch ~/.dotfiles/src/vscode/vscode-keybindings-before.jsonc
