567143bfb911a0e87e45e3ffd965ee3511ae94bb
Yunier Alvarez <yunieralvarez@gmail.com>
2025-03-02 20:43:19 -0500

    auto sync from primary repo

--- src/git/git-todo.sh
+++ src/git/git-todo.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [ ! -e "$HOME/Projects/com.gitlab.yunieralvarez.repo/repo/zig/top-down-game/TODO.md" ]; then
-  cp
-  ~/.dotfiles/src/git/todos/com.gitlab.yunieralvarez.repo/repo/zig/top-down-game/TODO.md \
+  cp \
+    ~/.dotfiles/src/git/todos/com.gitlab.yunieralvarez.repo/repo/zig/top-down-game/TODO.md \
     ~/Projects/com.gitlab.yunieralvarez.repo/repo/zig/top-down-game/TODO.md
 fi
 
