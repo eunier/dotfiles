dc14d6399c3b7b7e651bce787f35c3d9b7ff3001
Yunier Alvarez <yunieralvarez@gmail.com>
2025-03-02 21:12:44 -0500

    auto sync from primary repo

--- src/diff.sh
+++ src/diff.sh
@@ -3,17 +3,17 @@
 sh ~/.dotfiles/src/diff/diff-autostart.sh
 # sh ~/.dotfiles/src/diff/diff-dconf.sh
 sh ~/.dotfiles/src/diff/diff-doas.sh
-# sh ~/.dotfiles/src/diff/diff-fastfetch-config.sh
-# sh ~/.dotfiles/src/diff/diff-fastfetch-output.sh
+sh ~/.dotfiles/src/diff/diff-fastfetch-config.sh
+sh ~/.dotfiles/src/diff/diff-fastfetch-output.sh
 sh ~/.dotfiles/src/diff/diff-flatpak.sh
 sh ~/.dotfiles/src/diff/diff-git-repos.sh
 sh ~/.dotfiles/src/diff/diff-syncthing-config.sh
-# sh ~/.dotfiles/src/diff/diff-timeshift.sh
-# sh ~/.dotfiles/src/diff/diff-tree.sh
-# sh ~/.dotfiles/src/diff/diff-vscode-extensions.sh
-# sh ~/.dotfiles/src/diff/diff-vscode-keybindings.sh
-# sh ~/.dotfiles/src/diff/diff-vscode-settings.sh
-# sh ~/.dotfiles/src/diff/diff-zig-version-list.sh
-# sh ~/.dotfiles/src/diff/diff-dnf-copr-list.sh
-# sh ~/.dotfiles/src/diff/diff-dnf-info-installed.sh
-# sh ~/.dotfiles/src/diff/diff-dnf-repo-list.sh
+sh ~/.dotfiles/src/diff/diff-timeshift.sh
+sh ~/.dotfiles/src/diff/diff-tree.sh
+sh ~/.dotfiles/src/diff/diff-vscode-extensions.sh
+sh ~/.dotfiles/src/diff/diff-vscode-keybindings.sh
+sh ~/.dotfiles/src/diff/diff-vscode-settings.sh
+sh ~/.dotfiles/src/diff/diff-zig-version-list.sh
+sh ~/.dotfiles/src/diff/diff-dnf-copr-list.sh
+sh ~/.dotfiles/src/diff/diff-dnf-info-installed.sh
+sh ~/.dotfiles/src/diff/diff-dnf-repo-list.sh
