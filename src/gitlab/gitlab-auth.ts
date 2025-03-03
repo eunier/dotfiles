5ccfc76a87eb3e8d21faeaccbd642de92bf438ef
Yunier Alvarez <yunieralvarez@gmail.com>
2025-03-02 21:23:04 -0500

    auto sync from primary repo

--- src/gitlab/gitlab-auth.ts
+++ src/gitlab/gitlab-auth.ts
@@ -8,5 +8,5 @@ await $`echo "#!/usr/bin/env bash" > ~/.dotfiles/src/gitlab/gitlab-auth.sh`;
 if (status.includes("Unauthorized")) {
 	await $`echo "glab auth login" >> ~/.dotfiles/src/gitlab/gitlab-auth.sh`;
 } else {
-	await $`echo 'echo "Already login to gitlab cli"' >> ~/.dotfiles/src/gitlab/gitlab-auth.sh`;
+	await $`echo 'echo "Already logged in to GitLab CLI."' >> ~/.dotfiles/src/gitlab/gitlab-auth.sh`;
 }
