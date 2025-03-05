034fdf117f684cdda1a56ff20ea0150ff976098b
Yunier Alvarez <yunieralvarez@gmail.com>
2025-03-04 22:52:31 -0500

    auto sync from primary repo

--- src/flatpak/flatpak-uninstall.auto.sh
+++ src/flatpak/flatpak-uninstall.auto.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 
+flatpak uninstall io.github.nokse22.ultimate-tic-tac-toe org.gnome.World.Secrets
+flatpak install flathub io.github.nokse22.ultimate-tic-tac-toe org.gnome.World.Secrets
