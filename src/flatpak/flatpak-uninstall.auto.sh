e7a7c9cda6b71d2b885a597e8adb2c8ad7d80760
Yunier Alvarez <yunieralvarez@gmail.com>
2025-03-07 00:39:09 -0500

    auto sync from primary repo

--- src/flatpak/flatpak-uninstall.auto.sh
+++ src/flatpak/flatpak-uninstall.auto.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 
+flatpak uninstall io.github.Foldex.AdwSteamGtk io.github.mimoguz.TriPeaks-GDX org.nickvision.cavalier org.nickvision.money org.nickvision.tagger org.nickvision.tubeconverter
+flatpak install flathub io.github.Foldex.AdwSteamGtk io.github.mimoguz.TriPeaks-GDX org.nickvision.cavalier org.nickvision.money org.nickvision.tagger org.nickvision.tubeconverter
