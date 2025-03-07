f8eaa51912789176c5bdeb36c0b6bc13f5cbdf20
Yunier Alvarez <yunieralvarez@gmail.com>
2025-03-06 21:16:14 -0500

    auto sync from primary repo

--- src/dnf.sh
+++ src/dnf.sh
@@ -27,7 +27,7 @@ if ! dnf repolist | grep -q "copr:copr.fedorainfracloud.org:ilyaz:LACT"; then
     sudo dnf copr enable ilyaz/LACT
 fi
 
-if ! dnf copr list | grep -q "copr.fedorainfracloud.org:pgdev:ghostty"; then
+if ! dnf copr list | grep -q "copr.fedorainfracloud.org/pgdev/ghostty"; then
     sudo dnf copr enable pgdev/ghostty
 fi
 
