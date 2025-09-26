c4c0d16d659b9e08608974e0e9758cc8d03d7a58
Yunier Alvarez <yunieralvarez@gmail.com>
2025-09-26 05:28:53 -0400

    auto sync from primary repo

--- src/bash.sh
+++ src/bash.sh
@@ -3,5 +3,13 @@
 COMPUTER_MODEL=$(sudo dmidecode -s system-product-name | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
 REPO_PATH=~/.dotfiles
 
-cp ~/.bashrc ~/.dotfiles/src/files/after/home/~/.bashrc
-cp ~/.bash_profile ~/.dotfiles/src/files/after/home/~/.bash_profile
+BASHRC_DEST=~/.bashrc $REPO_PATH/src/files/"$COMPUTER_MODEL"/home/~/.bashrc
+mkdir -p "$(dirname "$BASHRC_DEST")"
+cp ~/.bashrc "$BASHRC_DEST"
+
+BASHRC_PROFILE_DEST=~/.bash_profile $REPO_PATH/src/files/"$COMPUTER_MODEL"/home/~/.bash_profile
+mkdir -p "$(dirname "$BASHRC_PROFILE_DEST")"
+cp ~/.bash_profile "$BASHRC_PROFILE_DEST"
+
+# cp ~/.bashrc ~/.dotfiles/src/files/after/home/~/.bashrc
+# cp ~/.bash_profile ~/.dotfiles/src/files/after/home/~/.bash_profile
