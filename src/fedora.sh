adeb02109c95353bae220777855cf844fb2431da
Yunier Alvarez <yunieralvarez@gmail.com>
2025-09-26 05:38:19 -0400

    auto sync from primary repo

--- src/fedora.sh
+++ src/fedora.sh
@@ -14,7 +14,7 @@ sh ~/.dotfiles/src/git.sh
 sh ~/.dotfiles/src/homebrew.sh
 
 sh ~/.dotfiles/src/autostart.sh
-# sh ~/.dotfiles/src/bash.sh
+sh ~/.dotfiles/src/bash.sh
 # sh ~/.dotfiles/src/dconf.sh
 # sh ~/.dotfiles/src/doas.sh
 sh ~/.dotfiles/src/fish.sh
