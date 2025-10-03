82e7b10f4461e6f70abaf9babefeda7e7472105f
Yunier Alvarez <yunieralvarez@gmail.com>
2025-10-03 04:28:29 -0400

    auto sync from primary repo

--- src/tree.sh
+++ src/tree.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-# touch ~/.dotfiles/src/tree/tree-filen-before.txt
-# tree -a ~/.config/@filen > ~/.dotfiles/src/tree/tree-filen-after.txt
-# tree -aDgNpqsu ~/.config/@filen >> ~/.dotfiles/src/tree/tree-filen-after.txt
+COMPUTER_MODEL=$(sudo dmidecode -s system-product-name | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+REPO_PATH=~/.dotfiles
 
-tree . -a -I ".git|node_modules" > ~/.dotfiles/src/tree/tree-output.txt
-
+tree . -a -I ".git|node_modules" >$REPO_PATH/src/tree/tree-output_"$COMPUTER_MODEL".txt
