8c26c6feceea1fac234a227709f3afc736016efe
Yunier Alvarez <yunieralvarez@gmail.com>
2025-10-05 05:57:37 -0400

    auto sync from primary repo

--- src/syncthing.sh
+++ src/syncthing.sh
@@ -3,8 +3,7 @@
 COMPUTER_MODEL=$(sudo dmidecode -s system-product-name | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
 REPO_PATH=~/.dotfiles
 
-touch $REPO_PATH/src/syncthing/syncthing-config-before.json
-syncthing cli config dump-json >$REPO_PATH/src/syncthing/syncthing-config-after.json
+syncthing cli config dump-json >$REPO_PATH/src/syncthing/syncthing-config_"$COMPUTER_MODEL".json
 
 cd $REPO_PATH || exit
 bun run syncthing:config:mask
--- src/syncthing/syncthing-config-mask.ts
+++ src/syncthing/syncthing-config-mask.ts
@@ -1,13 +1,20 @@
 import { $ } from "bun";
 
+let computerModel =
+  await $`sudo dmidecode -s system-product-name | tr '[:upper:]' '[:lower:]' | tr ' ' '-'`.text();
+
+computerModel = computerModel.replace("\n", "");
+
+const repoPath = "~/.dotfiles";
+
 const keys = ["apiKey", "deviceID", "encryptionPassword", "password", "user"];
-const path = "~/.dotfiles/src/syncthing/syncthing-config-after.json";
+const path = `${repoPath}/src/syncthing/syncthing-config_${computerModel}.json`;
 
 const data: Record<PropertyKey, unknown> =
-	await $`cat ~/.dotfiles/src/syncthing/syncthing-config-after.json`.json();
+  await $`cat ${repoPath}/src/syncthing/syncthing-config_${computerModel}.json`.json();
 
 const masked = JSON.stringify(data, (key, val) =>
-	keys.includes(key) ? "***" : val,
+  keys.includes(key) ? "***" : val
 );
 
 await $`echo ${masked} > ${path}`;
