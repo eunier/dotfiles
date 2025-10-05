3cbdc171ec1f489e05a9c118ee4f869b75ee6642
Yunier Alvarez <yunieralvarez@gmail.com>
2025-10-05 05:52:44 -0400

    auto sync from primary repo

--- src/sync.sh
+++ src/sync.sh
@@ -4,8 +4,4 @@
 # rm -rf /run/media/tron/External/KeePass
 # cp -r ~/Documents/Applications/KeePass /run/media/tron/External/KeePass/
 
-COMPUTER_MODEL=$(sudo dmidecode -s system-product-name | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
-REPO_PATH=~/.dotfiles
-
-
 rsync -av --delete ~/Documents/Applications/KeePass/ /run/media/tron/External/KeePass/
--- src/timeshift.sh
+++ src/timeshift.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-touch ~/.dotfiles/src/timeshift/timeshift-before.json
-cp /etc/timeshift/timeshift.json ~/.dotfiles/src/timeshift/timeshift-after.json
+COMPUTER_MODEL=$(sudo dmidecode -s system-product-name | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+REPO_PATH=~/.dotfiles
+
+cp /etc/timeshift/timeshift.json $REPO_PATH/src/timeshift/timeshift_"$COMPUTER_MODEL".json
