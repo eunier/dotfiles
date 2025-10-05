dd5ebc3e593b2da7b4f4bd9b5850f74b81e37b0c
Yunier Alvarez <yunieralvarez@gmail.com>
2025-10-05 05:48:24 -0400

    auto sync from primary repo

--- src/doas.sh
+++ src/doas.sh
@@ -5,6 +5,6 @@ REPO_PATH=~/.dotfiles
 
 echo "Creating doas config..."
 sudo touch /etc/doas.conf
-echo "permit tron as root" | sudo tee /etc/doas.conf >/dev/null
+echo "permit $USER as root" | sudo tee /etc/doas.conf >/dev/null
 
 sudo cp /etc/doas.conf $REPO_PATH/src/doas/doas_"$COMPUTER_MODEL".conf
--- src/sync.sh
+++ src/sync.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-# TODO switch to rsync
-rm -rf /run/media/tron/External/KeePass
-cp -r ~/Documents/Applications/KeePass /run/media/tron/External/KeePass/
+# # TODO switch to rsync
+# rm -rf /run/media/tron/External/KeePass
+# cp -r ~/Documents/Applications/KeePass /run/media/tron/External/KeePass/
+
+COMPUTER_MODEL=$(sudo dmidecode -s system-product-name | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+REPO_PATH=~/.dotfiles
+
+
+rsync -av --delete ~/Documents/Applications/KeePass/ /run/media/tron/External/KeePass/
