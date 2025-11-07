8d3868f9fb444749699cb79b529c23a206ff02df
Yunier Alvarez <yunieralvarez@gmail.com>
2025-11-07 06:09:29 -0500

    sync

--- src/rust.zig
+++ src/rust.zig
@@ -7,7 +7,13 @@ const log = std.log.scoped(.rust);
 
 pub fn sync(alc: mem.Allocator) !void {
     log.info("syncing", .{});
+    try install(alc);
     try update(alc);
+}
+
+fn install(alc: mem.Allocator) !void {
+    log.info("installing", .{});
+    _ = try sh.exec(alc, "rustup toolchain install stable", .{});
 }
 
 fn update(alc: mem.Allocator) !void {
