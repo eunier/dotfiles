a416501cf6cc67b89f848bd542b6f75813acc63a
Yunier Alvarez <yunieralvarez@gmail.com>
2025-11-11 04:23:55 -0500

    sync

--- src/neovim/config/lua/config/pack.lua
+++ src/neovim/config/lua/config/pack.lua
@@ -1,8 +1,10 @@
 vim.pack.add({
     { src = "https://github.com/BurntSushi/ripgrep" },
     { src = "https://github.com/folke/which-key.nvim" },
+    { src = "https://github.com/m4xshen/hardtime.nvim" },
     { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
     { src = "https://github.com/mason-org/mason.nvim" },
+    { src = "https://github.com/MunifTanjim/nui.nvim" },
     { src = "https://github.com/navarasu/onedark.nvim" },
     { src = "https://github.com/neovim/nvim-lspconfig" },
     { src = "https://github.com/nvim-lua/plenary.nvim" },
@@ -10,7 +12,9 @@ vim.pack.add({
     { src = "https://github.com/nvim-mini/mini.icons" },
     { src = "https://github.com/nvim-telescope/telescope.nvim" },
     { src = "https://github.com/nvim-tree/nvim-web-devicons" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter",  branch = "main" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", branch = "main" },
+    { src = "https://github.com/ThePrimeagen/vim-be-good" },
+    { src = "https://github.com/tris203/precognition.nvim" },
 })
 
 -- TODO: update
